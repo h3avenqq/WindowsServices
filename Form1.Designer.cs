703b7f912ed445760120766e8baac10cd99e66a9
Maxim Shamrev <shmarev082@gmail.com>
2022-04-22 01:41:47 +0300

    Init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Services
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvServices = new System.Windows.Forms.DataGridView();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.BtnStartStop = new System.Windows.Forms.Button();
+            this.BtnPauseContinue = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvServices)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvServices
+            // 
+            this.DgvServices.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.DgvServices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvServices.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.DgvServices.Location = new System.Drawing.Point(4, 32);
+            this.DgvServices.MultiSelect = false;
+            this.DgvServices.Name = "DgvServices";
+            this.DgvServices.ReadOnly = true;
+            this.DgvServices.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DgvServices.Size = new System.Drawing.Size(794, 417);
+            this.DgvServices.TabIndex = 0;
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Location = new System.Drawing.Point(4, 3);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.BtnRefresh.TabIndex = 1;
+            this.BtnRefresh.Text = "Refresh";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // BtnStartStop
+            // 
+            this.BtnStartStop.Location = new System.Drawing.Point(85, 3);
+            this.BtnStartStop.Name = "BtnStartStop";
+            this.BtnStartStop.Size = new System.Drawing.Size(75, 23);
+            this.BtnStartStop.TabIndex = 2;
+            this.BtnStartStop.Text = "Start/Stop";
+            this.BtnStartStop.UseVisualStyleBackColor = true;
+            this.BtnStartStop.Click += new System.EventHandler(this.BtnStartStop_Click);
+            // 
+            // BtnPauseContinue
+            // 
+            this.BtnPauseContinue.Location = new System.Drawing.Point(167, 3);
+            this.BtnPauseContinue.Name = "BtnPauseContinue";
+            this.BtnPauseContinue.Size = new System.Drawing.Size(96, 23);
+            this.BtnPauseContinue.TabIndex = 3;
+            this.BtnPauseContinue.Text = "Pause/Continue";
+            this.BtnPauseContinue.UseVisualStyleBackColor = true;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnPauseContinue);
+            this.Controls.Add(this.BtnStartStop);
+            this.Controls.Add(this.BtnRefresh);
+            this.Controls.Add(this.DgvServices);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvServices)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvServices;
+        private System.Windows.Forms.Button BtnRefresh;
+        private System.Windows.Forms.Button BtnStartStop;
+        private System.Windows.Forms.Button BtnPauseContinue;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Services
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            InitDataGrid();
+            GetServices();
+        }
+
+        private void InitDataGrid()
+        {
+            DgvServices.Columns.Add("Name", "Name");
+            DgvServices.Columns.Add("Id", "Id");
+            DgvServices.Columns.Add("Description", "Description");
+            DgvServices.Columns.Add("State", "State");
+            DgvServices.Columns.Add("CanStop", "CanStop");
+            DgvServices.Columns.Add("CanPauseAndContinue", "CanPauseAndContinue");
+        }
+
+        private void GetServices()
+        {
+            DgvServices.Rows.Clear();
+
+            ServiceController[] scServices;
+            scServices = ServiceController.GetServices();
+
+
+            Console.WriteLine("Services running on the local computer:");
+            foreach (ServiceController scTemp in scServices)
+            {
+
+                ManagementObject wmiService;
+                wmiService = new ManagementObject("Win32_Service.Name='" + scTemp.ServiceName + "'");
+                wmiService.Get();
+
+                DgvServices.Rows.Add(scTemp.ServiceName,wmiService["ProcessId"], wmiService["Description"], scTemp.Status, scTemp.CanStop?"Yes":"No", 
+                    scTemp.CanPauseAndContinue ? "Yes" : "No");
+                //https://docs.microsoft.com/ru-ru/windows/win32/cimwin32prov/win32-process - здесь можно узнать название ключа
+
+            }
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            GetServices();
+        }
+
+        private void StartStopService(string serviceName)
+        {
+            ServiceController service = new ServiceController(serviceName);
+
+            if ((service.Status.Equals(ServiceControllerStatus.Stopped)) ||
+
+                (service.Status.Equals(ServiceControllerStatus.StopPending)))
+
+                service.Start();
+
+            else service.Stop();
+        }
+
+        private void BtnStartStop_Click(object sender, EventArgs e)
+        {
+            if (DgvServices.SelectedRows.Count != 0)
+            {
+                string name = DgvServices.SelectedRows[0].Cells[0].Value.ToString();
+                StartStopService(name);
+            }
+        }
+
+        private void PauseContinueService(string serviceName)
+        {
+            ServiceController service = new ServiceController(serviceName);
+
+            if (service.CanPauseAndContinue &&
+                (service.Status.Equals(ServiceControllerStatus.Paused) ||
+                (service.Status.Equals(ServiceControllerStatus.PausePending))))
+
+                service.Continue();
+
+            else if (service.CanPauseAndContinue &&
+                (service.Status.Equals(ServiceControllerStatus.Running)))
+
+                service.Pause();
+
+            else
+                MessageBox.Show("Can't pause/continue");
+        }
+
+
+    }
+}
